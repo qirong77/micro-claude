46a4c284703e727c2bc0877af68e016134f64904
qirong77 <qirong7777@gmail.com>
2026-05-14 09:27:23 +0800

    feat: 功能开发 ✨

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,9 @@ import { MicaAgent } from './core/agent.js';
 import { AutoCompactPlugin } from './plugins/agent/auto-compact-plugin.js';
 import { ErrorHandlerPlugin } from './plugins/agent/error-handler-plugin.js';
 import { QuickCommandLogPlugin } from './plugins/quick-command/quick-command-log-plugin.js';
-import { ChatHistoryPlugin } from './plugins/ui/chat-history-plugin.js';
 
 await MicaAgent.usePlugin(new AutoCompactPlugin());
 await MicaAgent.usePlugin(new ErrorHandlerPlugin());
 await MicaAgent.usePlugin(new QuickCommandLogPlugin());
-await MicaAgent.usePlugin(new ChatHistoryPlugin());
 
 MicaAgent.ui.run();
